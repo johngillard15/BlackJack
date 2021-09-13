9b5348665bac6839a3f248d40d09dbe3e70fe2a1
johngillard15 <johngillard15@gmail.com>
2021-09-13 18:29:00 -0400

    fixed deck size printout

--- src/com/game/BlackJack.java
+++ src/com/game/BlackJack.java
@@ -10,14 +10,15 @@ public class BlackJack {
     public BlackJack(){
         deck = new StandardDeck();
 
-        //System.out.println("deck size: " + deck.pile.size());
+        System.out.printf("Deck size: %d cards\n", ((StandardDeck) deck).pile.size());
         System.out.println(deck);
+
         deck.shuffle();
     }
 
     public void play(){
         System.out.println();
-        
+
         for (int i = 0; i < 5; i++) {
             System.out.println(deck.deal());
         }
