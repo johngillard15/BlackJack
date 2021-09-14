01bfca55d2c4cca2cd42f7fa8fc24c8e09e652ba
johngillard15 <johngillard15@gmail.com>
2021-09-13 22:52:33 -0400

    updated Cheater deck to only take from deck

--- src/com/card/CheaterStandardDeck.java
+++ src/com/card/CheaterStandardDeck.java
@@ -4,27 +4,54 @@ import com.utilities.CLI;
 import com.utilities.Input;
 import com.utilities.UI;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CheaterStandardDeck extends StandardDeck {
 
     @Override
     public Card deal(){
-        UI.listerator(VALUES);
-        System.out.println("Value:");
-        int valueIndex = Input.getInt(1, VALUES.length) - 1;
-        System.out.println(VALUES[valueIndex]);
+        Card cheatCard = null;
 
-        System.out.println("of...");
+        boolean inDeck = false;
+        do{
+            UI.listerator(VALUES);
+            System.out.println("Value:");
+            int valueIndex = Input.getInt(1, VALUES.length) - 1;
+            System.out.println(VALUES[valueIndex]);
 
-        UI.listerator(SUITS);
-        System.out.print("Suit:");
-        int suitIndex = Input.getInt(1, SUITS.length) - 1;
-        System.out.println(SUITS[suitIndex]);
+            System.out.println("\nof...\n");
 
-        Card card = new Card(SUITS[suitIndex], VALUES[valueIndex]);
+            UI.listerator(SUITS); // TODO: fix spacing in output
+            System.out.println("Suit:");
+            int suitIndex = Input.getInt(1, SUITS.length) - 1;
+            System.out.println(SUITS[suitIndex]);
 
-        System.out.printf("The dealer slides you a%s %s...\n", valueIndex == 0 ? "n" : "", card);
-        CLI.pause();
+//            try{
+//                cheatCard = pile.stream()
+//                        .filter(card -> card.suit.equals(SUITS[suitIndex]) && card.value.equals(VALUES[valueIndex]))
+//                        .collect(Collectors.toList())
+//                        .get(0);
+//                inDeck = pile.remove(cheatCard);
+//
+//                System.out.printf("The dealer slides you a%s %s...\n", valueIndex == 0 ? "n" : "", cheatCard);
+//            }
+//            catch(IndexOutOfBoundsException e){
+//                System.out.println("We don't have that card, pick another one...");
+//            }
 
-        return card;
+            cheatCard = new Card(SUITS[suitIndex], VALUES[valueIndex]);
+            inDeck = pile.removeIf(thisCard -> thisCard.suit.equals(SUITS[suitIndex]) && thisCard.value.equals(VALUES[valueIndex]));
+            // there should only be one of each card so this^ is fine
+
+            if(inDeck)
+                System.out.printf("The dealer slides you the %s...\n", cheatCard);
+            else
+                System.out.println("We don't have that card, pick another one...");
+
+            CLI.pause();
+        }while(!inDeck);
+
+        return cheatCard;
     }
 }
